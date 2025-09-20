3a98a2e59288d2fe180129511359a2ff40400380
sunny <goodluck14115@gmail.com>
2025-09-20 12:39:58 +0530

    chore: setup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <h1 className='text-3xl font-bold text-blue-500'>Hello World</h1>
+    <div className="flex h-screen items-center justify-center flex-col">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+      <Button>Button</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
